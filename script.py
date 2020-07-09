853e0ee7b1118a2dd718361c2ee21e39d4201869
ritik <58474947+hd639292@users.noreply.github.com>
2020-07-08 21:59:00 -0700

    new changes

--- script.py
+++ script.py
@@ -4,48 +4,50 @@ from googleapiclient.discovery import build
 
 import time
 
-stts = [0]
+old_view_count = 0
+api_key = "your token"
 
-api_key = "Your youtube data api token key"
 flow = InstalledAppFlow.from_client_secrets_file(
-    #name of your oauth credential files ,please make that this file is in same directory
     'client_secret.json',
-    #specific scopes url are defined for youtube data api related actions each action have specific action
-    #this scopes let you edit , delete your youtube video
     scopes=['https://www.googleapis.com/auth/youtube.force-ssl'])
 
 credentials = flow.run_console()
 
-youtube = build('youtube' , 'v3' ,credentials = credentials ,developerKey = api_key)
+youtube = build('youtube' , 'v3' , developerKey = api_key ,credentials = credentials)
 
-#this function return the views count 
 def views_count():
-    request = youtube.videos().list(id='Youtube video id',part="statistics")
-    pop = dict(request.execute())
-    views = pop['items'][0]["statistics"]['viewCount']
-    return views
+    request = youtube.videos().list(id="video id",part="statistics")
+    view_out = dict(request.execute())
+    total_count = view_out['items'][0]["statistics"]['viewCount']
+    return total_count
+
 
 def changing_title():
-    new_title = int(views_count())
-    if new_title[0] > stts[0]:
+    global old_view_count
+    new_view_count = int(views_count())
+    if new_view_count > old_view_count :
         change = youtube.videos().update(part="snippet",body={
-            #id of your youtube video of which title will change 
-            "id": "h9N29ZLDGY0",
+            "id": "video id",
             "snippet": {
     	   	"categoryId": 24,
     		"defaultLanguage": "en",
     		"description": "Salman saved his culture.",
-    		"title": "Salman Khan slapped " +str(new_title[0])+ " times."
+    		"title": "Salman Khan slapped a women " +format(new_view_count , ',')+ " times "
     	    }
     	  })
         change.execute()
-        stts[0] = new_title[0]
+        old_view_count = new_view_count
+        print("title updated")
     else:
-        print("No changed exists")
-#use while true for running it continuously
-for t in range(10):
+        pass
+
+    runtime = True
+try:
+   while True:
     changing_title()
-    print(stts[0])
     time.sleep(900)
+except Exception as e:
+    time.sleep()
+
     
 
